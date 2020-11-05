b5822bd4b15bae03a1c9110922dee0a0afec210d
Mockyy <32643968+Mockyy@users.noreply.github.com>
2020-11-05 16:53:34 +0100

    Singleton
    Mise en place d'un singleton

---
+++ SingletonFabriqueAbstraite/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text = default;
+
+    private void Update()
+    {
+        Singleton.Instance.IncreaseScore();
+        text.text = Singleton.Instance.GetScore().ToString();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ SingletonFabriqueAbstraite/Assets/Script/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private int score;
+
+    public static Singleton Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
